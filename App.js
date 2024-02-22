1d2e3c0670dc32150b7a8de071226b4b926dae74
OtavioSantos <otavio.santos126@etec.sp.gov.br>
2024-02-22 10:44:57 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Otavio Santos</Text>
+      <Text>Desenvolvimento de Dispositivos Móveis - 22/02/24</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
